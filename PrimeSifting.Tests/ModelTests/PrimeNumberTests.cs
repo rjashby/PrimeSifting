d05a606e8b807d062dde35b3f996929f0ce43cee
Ryan Ashby <rjapsu160@gmail.com>
2022-02-23 11:51:43 -0800

    create method to create list of nums
    Co-authored-by: S-Espinet <s.lou.espinet@gmail.com>

---
+++ PrimeSifting.Tests/ModelTests/PrimeNumberTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeSifting;
+
+namespace PrimeSifting.Tests 
+{
+  [TestClass]
+  public class PrimeNumber
+  {
+    [TestMethod]
+    public void ListCreate_
+  }
+}
--- PrimeSifting/Models/PrimeNumber.cs
+++ PrimeSifting/Models/PrimeNumber.cs
@@ -2,6 +2,19 @@ namespace PrimeSifting
 {
   public class PrimeNumber
   {
-    
+    public bool CreateList(int num) 
+    {
+      List<int> myNums = new List<int> {};
+      while (num >= 2)
+      {
+        myNums.Add(num);
+        num = num - 1;
+      }
+      myNums.Reverse();
+      foreach (int shoes in myNums) 
+      {
+        Console.WriteLine(shoes);
+      }
+    }
   }
 }
